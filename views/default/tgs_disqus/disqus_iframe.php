b313c5c0b5ecea208374af353728e2647e1ca7d6
Jeff Tilson <jtilson@thinkglobalschool.com>
2012-11-06 17:00:39 -0500

    Using combinaton of site title and entity guid for disqus_title

--- views/default/tgs_disqus/disqus_iframe.php
+++ views/default/tgs_disqus/disqus_iframe.php
@@ -33,8 +33,8 @@ $css = elgg_get_loaded_external_files('css', 'disqus_iframe');
 
 // Page title
 $title = elgg_get_config('sitename');
-$title .= ": " . ($entity->name ? $entity->name : $entity->title);
-
+//$title .= ": " . ($entity->name ? $entity->name : $entity->title);
+$title .= ': ' . $entity->guid; // Using the guid for title, as titles can technically change!
 ?>
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
